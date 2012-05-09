59cee4e189917a81b1648f996c86313c7d118f4a
Tanc <mail@tanc.me>
2012-05-10 08:48:40 +1000

    Initial commit of plugin code and updated README.

---
+++ DPCompletions.py
@@ -0,0 +1,103 @@
+import os, fnmatch, re, threading, sublime, sublime_plugin
+
+class ProjectCompletionsScan(threading.Thread):
+
+    def __init__(self, rootPath, timeout):
+        threading.Thread.__init__(self)
+        self.rootPath = rootPath
+        self.timeout = timeout
+        self.result = None
+
+    def run(self):
+        try:
+            patterns = ['.inc', '.php', '.module']
+            search = re.compile(r'^function\s(.+?)\((?:(.+?))?\)\s{$', re.MULTILINE)
+            compPath = os.path.dirname(self.rootPath) + '/Drupal.sublime-projectcompletions'
+            cfp = open(compPath, 'w')
+            cfp.close()
+            cfp = open(compPath, 'a')
+
+            for root, dirs, files in os.walk(os.path.dirname(self.rootPath)):
+                for p in patterns:
+                    for f in files:
+                        if f.endswith(p):
+                            # Open the file.
+                            fp = open(os.path.join(root, f), 'r')
+                            content = fp.read()
+                            # Retrieve functions from file.
+                            funcs = search.findall(content)
+                            for row in funcs:
+                                args = ''
+                                if row[1]:
+                                    i = 0
+                                    arglist = row[1].replace(', ', ',').split(',')
+                                    for i, val in enumerate(arglist):
+                                        #arglist[i] = '${' + str(i + 1) + ':' + arglist[i].replace('$', '\$') + '}'
+                                        arglist[i] = '${%s:%s}'   % (i, arglist[i].replace('$', '\$'))
+                                    args = '(%s)'   % (', '.join(arglist))
+                                else:
+                                    args = '()'
+                                line = '%s\t%s%s' % (row[0], row[0], args)
+                                # Append to file
+                                cfp.write(line + "\n")
+                            fp.close()
+            cfp.close()
+            return
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+
+
+class ProjectCompletions(sublime_plugin.EventListener):
+
+    def find_file(self, start_at, look_for):
+        start_at = os.path.abspath(start_at)
+        if not os.path.isdir(start_at):
+            start_at = os.path.dirname(start_at)
+        while True:
+            for filename in os.listdir(start_at):
+                if fnmatch.fnmatch(filename, look_for):
+                    return os.path.join(start_at, filename) 
+            continue_at = os.path.abspath(os.path.join(start_at, '..'))
+            if continue_at == start_at:
+                return None
+            start_at = continue_at
+    
+    def on_post_save(self, view):
+        path = view.file_name()
+        rootPath = None
+
+        if path:
+            # Try to find the myproject.sublime-project file
+            for filename in ['*.sublime-project']:
+                rootPath = self.find_file(path, filename)
+        if rootPath:
+            threads = []
+            thread = ProjectCompletionsScan(rootPath, 5)
+            threads.append(thread)
+            thread.start()
+
+    def on_query_completions(self, view, prefix, locations):
+        path = view.file_name()
+        completions_location = None
+        if path:
+            # Try to find the Drupal.sublime-completions file
+            for filename in ['*.sublime-projectcompletions']:
+                completions_location = self.find_file(path, filename)
+        if completions_location:
+            fp = open(completions_location, 'r')
+
+            t = ()
+            data = []
+            line = fp.readline()
+
+            while len(line) != 0:
+               e1, e2 = line.split("\t")
+               t = e1, e2.rstrip()
+               data.append(t)
+               line = fp.readline()
+               
+            fp.close()
+            return data
+        else:
+            return None
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-st2-drupal-autocomplete
+Sublime Text 2 Drupal autocomplete plugin
 =======================
 
-A Sublime Text 2 plugin which provides autocomplete for all Drupal functions.
+This is a package manager hosted plugin for Sublime Text 2 which provides autocomplete for all Drupal functions on a per project basis. This means that you can have this plugin autocomplete your Drupal 7 project's functions and provide argument hinting as well as your Drupal 6 project with no clashes. It manages this by scanning your project's files and creating a file in your project's root directory called Drupal.sublime-projectcompletions containing Sublime Text 2's autocomplete data. The plugin then injects this into ST2's autocomplete using the API and the result should be autocomplete suggestions for all functions in your project.
+
+This is a first attempt at providing this functionality as a plugin and my first attempt at writing code in Python. Any bugs or suggestions for improvement (ideally with a coded solution) please add comments in the issue queue.
